83005d6e1b9c94c3318d2bbffcb655c04cab2810
Nghiaduc <nghiaduc196@gmail.com>
2022-12-24 13:39:00 +0700

    update bug 5 + modal

--- asset/js/script.js
+++ asset/js/script.js
@@ -26,24 +26,38 @@ $(document).ready(function () {
     // Bind event listener
     $(window).resize(checkWidth);
 
-    $(".input-checkbox:checkbox").change(function () {
-        if ($(this).is(':checked') === true) {
-            $(this).parent().addClass('active');
-        } else {
-            $(this).parent().removeClass('active');
-        }
-    });
+    $('body').on('click', '.submit-button', function (){
+        event.preventDefault();
+    })
 
-    $(".input-checkbox:radio").change(function () {
-        let child = $(this).parent().parent().siblings();
-        $('.form-checkbox', child).removeClass('active');
-        $(this).parent().addClass('active');
-    });
+    $('body').on('click', '.reset-button', function (){
+        let form = $(this).parent().parent().parent();
+        if ($('.input-checkbox', form).is(':checked')){
+            $('.input-checkbox', form).parent().removeClass('active');
+        }
+    })
+
+    function ActiveCheck(){
+        $(".input-checkbox:checkbox").change(function () {
+            if ($(this).is(':checked') === true) {
+                $(this).parent().addClass('active');
+            } else {
+                $(this).parent().removeClass('active');
+            }
+        });
+
+        $(".input-checkbox:radio").change(function () {
+            let child = $(this).parent().parent().siblings();
+            $('.form-checkbox', child).removeClass('active');
+            $(this).parent().addClass('active');
+        });
+    };
+    ActiveCheck();
 
     $('body').on('click', '.icon-show', function () {
         $(this).toggleClass('show');
     })
-    $('body').on('click', '.text-show-collapse', function (){
+    $('body').on('click', '.text-show-collapse', function () {
         $(this).prev().toggleClass('show');
     });
 
@@ -51,15 +65,15 @@ $(document).ready(function () {
         $(this).parent().parent().remove();
     })
 
-    $('body').on('click', '.tab-scroll', function (){
+    $('body').on('click', '.tab-scroll', function () {
         let id = $(this).attr('data-id');
-        if (id == 1){
+        if (id == 1) {
             $(window).scrollTop($('#tab1').offset().top - 160);
         }
-        if (id == 2){
+        if (id == 2) {
             $(window).scrollTop($('#tab2').offset().top - 160);
         }
-        if (id == 3){
+        if (id == 3) {
             $(window).scrollTop($('#tab3').offset().top - 160);
         }
     });
@@ -67,14 +81,14 @@ $(document).ready(function () {
     $('body').on('click', '.icon-add-input', function () {
         $(this).parent().prev().append(`<div class="row row-cols-1 row-cols-lg-2 mt-3 gx-0">
                                             <div class="col w-calc-303">
-                                                <div class="position-relative">
-                                                    <img class="position-absolute right-8 top-7" src="./asset/image/icon_select_arr_bottom.svg" />
-                                                    <select style="z-index: 1" class="text-black ff-nt-sans fst-normal letter-spacing-10 form-control h-5 border-input" >
-                                                        <option>リストから選択</option>
-                                                        <option>リストから選択</option>
-                                                        <option>リストから選択</option>
-                                                        <option>リストから選択</option>
-                                                    </select>
+                                                <div class="custom-select position-relative">
+                                                        <select style="z-index: 1" class="border-radius-5 ff-nt-sans fst-normal letter-spacing-10 form-control h-5 border-input">
+                                                            <option class="text-color-gray-dark" value="0">リストから選択</option>
+                                                            <option>リストから選択</option>
+                                                            <option>リストから選択</option>
+                                                            <option>リストから選択</option>
+                                                            <option>リストから選択</option>
+                                                        </select>
                                                 </div>
                                             </div>
                                             <div class="col d-flex w-303 align-items-center">
@@ -83,6 +97,7 @@ $(document).ready(function () {
                                                 <img class="cursor-pointer ms-20 sub-input" src="./asset/image/icon_sub.svg" />
                                             </div>
                                         </div>`);
+        changeSelect();
     })
 
     $('body').on('click', '.icon-add-input-1', function () {
@@ -137,10 +152,14 @@ $(document).ready(function () {
 
         })
 
-        $('.icon-add-input-1').each(function (e, value) {}).click(false);
-        $('.icon-add-input-2').each(function (e, value) {}).click(false);
-        $('.icon-add-input').each(function (e, value) {}).click(false);
-        $('.sub-input').each(function (e, value) {}).click(false);
+        $('.icon-add-input-1').each(function (e, value) {
+        }).click(false);
+        $('.icon-add-input-2').each(function (e, value) {
+        }).click(false);
+        $('.icon-add-input').each(function (e, value) {
+        }).click(false);
+        $('.sub-input').each(function (e, value) {
+        }).click(false);
 
         $('.form-checkbox').each(function (e, value) {
             $('.notification').removeClass('visually-hidden')
@@ -178,27 +197,39 @@ $(document).ready(function () {
             $(value).removeClass('sub-input');
         });
 
-        $('.form-checkbox-custom').each(function (e, value){
+        $('.form-checkbox-custom').each(function (e, value) {
             $('input', $(this))[0].setAttribute("disabled", "");
-            if ($('input',value)[0].checked){
+            if ($('input', value)[0].checked) {
                 value.style.background = "#A2A2A2";
                 $('.icon', value).css('background', '#5F5D5D');
-            }else  {
+            } else {
                 value.style.background = "#A2A2A2";
             }
+        });
+
+        $('.select-selected').each(function (e, value) {
+            if ($('.same-as-selected', $(value).next()).length){
+                value.style.border = "2px solid #003B90";
+                value.style.background = "#F5F5F5";
+            }
+            value.addEventListener('click', function (){
+                console.log('hung test 1')
+            });
+
+            $(value).parent()[0].addEventListener('click', function (){});
         });
     })
 
 
     // datepicker-custom
 
-    $('body').on('click', '.datepicker-custom', function (){
+    $('body').on('click', '.datepicker-custom', function () {
         $('#modal-backdrop').addClass('modal-backdrop');
         $('body').addClass('overflow-hidden');
         $('.modal-content').css('opacity', 0.7);
     });
 
-    $('body').on('click', '.modal-backdrop', function (){
+    $('body').on('click', '.modal-backdrop', function () {
         $('#modal-backdrop').removeClass('modal-backdrop');
         $('body').removeClass('overflow-hidden');
         $('.modal-content').css('opacity', '1');
@@ -210,12 +241,11 @@ $(document).ready(function () {
     //     $('.modal-content').css('opacity', '1');
     // });
 
-    $('body').on('change', '.datepicker-custom', function (){
+    $('body').on('change', '.datepicker-custom', function () {
         $('#modal-backdrop').removeClass('modal-backdrop');
         $('body').removeClass('overflow-hidden');
         $('.modal-content').css('opacity', '1');
     });
-
 
 
     var availableTags = [
@@ -237,4 +267,81 @@ $(document).ready(function () {
             $("#tags").val(ui.item.label);
         }
     });
+
+
+    // custom select option
+    function changeSelect() {
+        var x, j, l, ll, selElmnt, a, b, c;
+        x = document.getElementsByClassName("custom-select");
+        l = x.length;
+        $('.custom-select').each(function (i, e){
+            if (!$('.select-selected', e)[0]){
+                selElmnt = x[i].getElementsByTagName("select")[0];
+                ll = selElmnt.length;
+                a = document.createElement("DIV");
+                a.setAttribute("class", "select-selected");
+                a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+                x[i].appendChild(a);
+                b = document.createElement("DIV");
+                b.setAttribute("class", "select-items select-hide");
+                for (j = 1; j < ll; j++) {
+                    c = document.createElement("DIV");
+                    c.innerHTML = selElmnt.options[j].innerHTML;
+                    c.addEventListener("click", function (e) {
+                        var y, i, k, s, h, sl, yl;
+                        s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+                        sl = s.length;
+                        h = this.parentNode.previousSibling;
+                        for (i = 0; i < sl; i++) {
+                            if (s.options[i].innerHTML == this.innerHTML) {
+                                s.selectedIndex = i;
+                                h.innerHTML = this.innerHTML;
+                                y = this.parentNode.getElementsByClassName("same-as-selected");
+                                yl = y.length;
+                                for (k = 0; k < yl; k++) {
+                                    y[k].removeAttribute("class");
+                                }
+                                this.setAttribute("class", "same-as-selected");
+                                break;
+                            }
+                        }
+                        h.click();
+                    });
+                    b.appendChild(c);
+                }
+                x[i].appendChild(b);
+                a.addEventListener("click", function (e) {
+                    e.stopPropagation();
+                    closeAllSelect(this);
+                    this.nextSibling.classList.toggle("select-hide");
+                    this.classList.toggle("select-arrow-active");
+                });
+            }
+
+        });
+    }
+
+    changeSelect();
+
+    function closeAllSelect(elmnt) {
+        var x, y, i, xl, yl, arrNo = [];
+        x = document.getElementsByClassName("select-items");
+        y = document.getElementsByClassName("select-selected");
+        xl = x.length;
+        yl = y.length;
+        for (i = 0; i < yl; i++) {
+            if (elmnt == y[i]) {
+                arrNo.push(i)
+            } else {
+                y[i].classList.remove("select-arrow-active");
+            }
+        }
+        for (i = 0; i < xl; i++) {
+            if (arrNo.indexOf(i)) {
+                x[i].classList.add("select-hide");
+            }
+        }
+    }
+
+    document.addEventListener("click", closeAllSelect);
 });
